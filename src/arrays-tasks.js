ce6d332e539de8b8c18f9e10f5f6283988640f47
Marta <emeraldraven27@gmail.com>
2024-12-23 01:21:50 +0300

    feat: add calculateBalance and createChunks solutions

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -328,8 +328,9 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  const sumElements = arr.map((el) => el.reduce((acc, val) => acc - val));
+  return sumElements.reduce((acc, val) => acc + val);
 }
 
 /**
@@ -344,8 +345,9 @@ function calculateBalance(/* arr */) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  const func = [...arr].map(() => arr.splice(0, chunkSize));
+  return func.filter((el) => el.length > 0);
 }
 
 /**
